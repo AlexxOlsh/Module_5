043b36f5c3f93a349baa79897207a564a52daa99
sagirov-coder006 <coder006@noproblemsite.ru>
2024-12-06 01:04:01 +0300

    Add actions Update settings

--- conf/settings.py
+++ conf/settings.py
@@ -3,15 +3,10 @@ from pathlib import Path
 from datetime import timedelta
 from dotenv import load_dotenv
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 dot_env = os.path.join(BASE_DIR, '.env.dev')
 load_dotenv(dotenv_path=dot_env)
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "127.0.0.1").split(" ")
